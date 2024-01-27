888615d462520f8ff06e0a027f89765d40563971
Maxim Shyryaev <ipz223_shmye@student.ztu.edu.ua>
2024-01-27 09:18:26 +0200

    Додано файли .gitignore і ігноровані файли

---
+++ Task0VS/Task0VS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2;
+        Console.WriteLine($"Сума чисел: {sum}");
+    }
+}
